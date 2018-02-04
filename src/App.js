5e445e4f5cb8c0f1d65d0f8add3ecc71649c67b2
runelynx <aboeger@gmail.com>
2018-02-03 21:44:14 -0800

    Make use of props; prepare for eventual implementation of the Yelp API

--- src/App.js
+++ src/App.js
@@ -3,6 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
+import Business from './components/Business/Business';
+
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [<Business />,
+  <Business />,
+  <Business />,
+  <Business />,
+  <Business />,
+  <Business />
+]; 
 
 class App extends Component {
   render() {
@@ -10,7 +31,7 @@ class App extends Component {
       <div className="App">
       <h1>ravenous</h1>
       <SearchBar />
-      <BusinessList />
+      <BusinessList businesses={businesses}/>
       </div>
     );
   }
