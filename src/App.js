71ed376d049d523a410c87dc0bee08c885071c4f
Andrew Boeger <aboeger@gmail.com>
2018-02-04 19:41:21 -0800

    Updating with project part 3 results

--- src/App.js
+++ src/App.js
@@ -26,11 +26,22 @@ const businesses = [<Business />,
 ]; 
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp for ${term}, ${location}, and ${sortBy}`);
+    
+  }
+
   render() {
     return (
       <div className="App">
       <h1>ravenous</h1>
-      <SearchBar />
+      <SearchBar searchYelp={this.searchYelp}/>
       <BusinessList businesses={businesses}/>
       </div>
     );
